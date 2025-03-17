fa83eb83414a1798a83fe95c03c2d81a893d4ef4
Supun Nethsara <supunmax663@gmail.com>
2025-03-17 07:01:45 -0700

    Implemented client registration with form validation and submission

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+function clientregister(Request $request)
+{
+    $request->validate([
+        'fname' => 'required|string|max:255',
+        'lname' => 'required|string|max:255',
+        'mobile' => 'required|string|max:15',
+        'email' => 'nullable|email|max:255|unique:clients,email',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+    $client = Client::create([
+        'first_name' => $request->fname,
+        'last_name' => $request->lname,
+        'mobile' => $request->mobile,
+        'email' => $request->email,
+        'password' => $request->password,
+    ]);
+    return response()->json([
+        'message' => 'Registration successful!',
+        'data' => $client,
+    ], 201);
+}
 }
---
+++ app/Models/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $table = 'clients';
+    protected  $fillable = [
+        'first_name',
+        'last_name',
+        'mobile',
+        'email',
+        'password',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($client) {
+            $client->password = bcrypt($client->password);
+        });
+    }
+}
