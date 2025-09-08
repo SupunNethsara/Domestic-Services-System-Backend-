654dd5795cde94f8fb971f890202aa70d94c8ea1
Supun Nethsara <supunmax663@gmail.com>
2025-09-08 06:52:51 -0700

    Make model to store client requested job data

--- app/Models/AvailableJobs.php
+++ app/Models/AvailableJobs.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class AvailableJobs extends Model
 {
-    //
+    protected $table = 'available_jobs';
+
+    protected $fillable = [
+        'id',
+        'title',
+        'category',
+        'message',
+    ];
 }
